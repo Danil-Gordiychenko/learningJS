6d1c94e0bf112869fdd2656b9023456239ed7e3b
Danil-Gordiychenko <d.p.gordiychenko@student.khai.edu>
2023-03-26 16:56:01 -0400

    math operations

---
+++ JavaScriptBasic/script.js
@@ -0,0 +1,28 @@
+const firstNumber = 50;
+const secondNumber = 4;
+let result;
+
+//Simple math operations 
+result = firstNumber + secondNumber;
+result = firstNumber - secondNumber;
+result = firstNumber * secondNumber;
+result = firstNumber / secondNumber;
+result = firstNumber % secondNumber;
+
+//Math object 
+result = Math.PI;
+result = Math.round(3.5);
+result = Math.ceil(3.3);
+result = Math.floor(3.7);
+result - Math.sqrt(9);
+result = Math.abs(-7);
+result = Math.pow(2, 3);
+result = Math.min(3, 33, 82, -1);
+result = Math.max(3, 33, 82, -1);
+result = Math.floor(Math.random() * 100);
+
+
+
+
+
+console.log(result);

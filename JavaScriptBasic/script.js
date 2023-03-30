503530ce74bd4893e640dc83d07d30064ad1fa98
Danil-Gordiychenko <d.p.gordiychenko@student.khai.edu>
2023-03-30 17:06:49 -0400

    practice arrays

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -34,4 +34,66 @@
 // const enyItems = [4, true, NaN, 'string', new Date()];
 // console.log(enyItems);
 
-const numbers = [];
+// const numbers = [1 , 23,  3, 81];
+// let result;
+
+
+// result = numbers.length;
+// result = Array.isArray(numbers);
+// result = numbers.indexOf(81);
+
+// //push pop()
+// result = numbers.push('7');
+// result = numbers.pop();
+
+// //unshift() shift()
+// result = numbers.unshift(2);
+// result = numbers.shift();
+// result = numbers.push('1', 2, 3);
+// result = numbers.unshift(98, 'some');
+
+// // splice()
+// result = numbers.splice(3, 4);
+
+// // reverse()
+// result = numbers.reverse();
+
+// //slice
+// result = numbers.slice(1, 3);
+
+// //concat
+// result = numbers.concat(result);
+
+
+// console.log(numbers);
+// console.log(result);
+
+// 'use strict';
+
+
+// const arrayBills = [20, 1, 8, 32, 2];
+// const totalBillsAndTips = [];
+// const numberArrayBills = arrayBills[2];
+
+
+// const calculateTips = (tips) => tips < 20 ? tips * 0.2 : tips * 0.15;
+
+// function addToTotalBills () {
+//     const callCalculateTips = calculateTips(numberArrayBills);
+//     totalBillsAndTips.push(numberArrayBills, callCalculateTips);
+// }
+
+
+// addToTotalBills ();
+// console.log(totalBillsAndTips);
+
+
+// const calculateTips = (tips) => tips < 20 ? tips * 0.2 : tips * 0.15;
+
+// const arrayBills = [20, 1, 8];
+// const arrayTips = [calculateTips(arrayBills[0]), calculateTips(arrayBills[1]), calculateTips(arrayBills[2])];
+// const totalBillsAndTips = [calculateTips(arrayBills[0]) + arrayBills[0], calculateTips(arrayBills[1]) + arrayBills[1], calculateTips(arrayBills[2]) + arrayBills[2]];
+// const numberArrayBills = arrayBills[2];
+
+// console.log(arrayTips);
+// console.log(arrayBills, totalBillsAndTips);

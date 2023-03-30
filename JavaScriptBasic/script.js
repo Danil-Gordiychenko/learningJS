36d05c2446582190bd5189ae839d946c57f3f8ea
Danil-Gordiychenko <d.p.gordiychenko@student.khai.edu>
2023-03-30 14:13:14 -0400

    arrays basic

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,2 +1,37 @@
-let x = 'some';
-console.log(x);
+// const color1 = 'red';
+// const color2 = 'orange';
+// const color3 = 'yellow';
+// const color4 = 'green';
+// const color5 = 'blue';
+// const color6 = 'indigo';
+// const color7 = 'violet';
+
+// const rainbowColors = ['red', 'arange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// console.log(rainbowColors[4]);
+// console.log('the sky is ' + rainbowColors[4]);
+
+// rainbowColors[5] = 'dark blue';
+// rainbowColors[rainbowColors.length] = 'indigo';
+
+// console.log(rainbowColors);
+
+// // const stringNumbers = [];
+// // stringNumbers[0] = 'one';
+// // stringNumbers[1] = 'two';
+// // stringNumbers[2] = 'three';
+// // stringNumbers[3] = 'four';
+// // stringNumbers[4] = 'five';
+
+// const stringNumbers = new Array('one', 'two', 'three');
+
+// console.log(stringNumbers);
+
+// const numbers = new Array(1, 2, 3, 4);
+
+// console.log(numbers);
+
+// const enyItems = [4, true, NaN, 'string', new Date()];
+// console.log(enyItems);
+
+const numbers = [];

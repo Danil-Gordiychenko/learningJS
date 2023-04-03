f91f6e6381da83573292d432288c8df065aeeba8
Danil-Gordiychenko <d.p.gordiychenko@student.khai.edu>
2023-04-03 19:58:41 -0400

    practice js

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,10 +1,14 @@
-// 'use strict';
+'use strict';
 
 // const list = {
 //   'create a new practice task': 'In Progress',
 //   'make a bed': 'Done',
 //   'write a post': 'To Do',
 // };
+
+// let some = 1;
+
+// console.log(some += 1);
 
 // function changeStatus(prop, status) {
 //   list[prop] = status;
@@ -81,3 +85,69 @@
 // const sum = (a, b) => a + b;
 
 // console.log(sum(1, 2));
+
+// const some = [1, 2, 3];
+
+// const humidities = [
+//   32,
+//   45,
+//   29,
+//   19,
+//   'error',
+//   58,
+//   71,
+//   47,
+//   33,
+//   'error',
+//   42,
+//   51,
+//   49,
+// ];
+
+// for (let i = 0; i < humidities.length; i++) {
+//   if (typeof humidities[i] !== 'number') {
+//     humidities.splice(i, 1);
+//   }
+// }
+
+// const calculateHumidities = function () {
+//   const minValue = Math.min(...humidities);
+//   const maxValue = Math.max(...humidities);
+
+//   return maxValue - minValue;
+// };
+
+// console.log(calculateHumidities());
+
+// const calculateHumiditiesAmplitide = function (h1, h2) {
+//   const humidities = h1.concat(h2);
+//   let max = humidities[0];
+//   let min = humidities[0];
+
+//   for (let i = 0; i < humidities.length; i++)
+//     if (typeof humidities[i] !== 'number') continue;
+//     else if (max < humidities[i]) {
+//       max = humidities[i];
+//     } else if (min > humidities[i]) {
+//       min = humidities[i];
+//     }
+
+//   return max - min;
+// };
+
+// console.log(
+//   calculateHumiditiesAmplitide([1, '0', 2, 3, 4, 'some string'], [10])
+// );
+
+// let num =+ prompt('Enter a number');
+
+// console.log(isFinite(num));
+// console.log(Number('123.123'));
+// console.log(+'123.123');
+
+// console.log(parseInt('100px'));
+// console.log(parseFloat('100.345.123'));
+// console.log(parseInt('a123'));
+// console.log(parseFloat('a123'));
+
+// console.log(7e1);
